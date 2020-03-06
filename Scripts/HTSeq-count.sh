40a2d3fbb1628e841a8b4db9e875521f1a1fa6bb
Inês Batista <41570705+ines-batista@users.noreply.github.com>
2020-03-06 15:06:37 +0000

    HTSeq-count.sh update
    Removal of features that do not exist in GTF format

--- Scripts/HTSeq-count.sh
+++ Scripts/HTSeq-count.sh
@@ -8,53 +8,37 @@ files="${@:3}"
 
 ## 1. HTSeq-count tables
 echo
-echo "Building HTSeq-count tables for $files files"
+
 
 #-t Feature type
 #--stranded=no because we have not collected data with a strand-specific protocol
 #-i ID attribute with which it will filter/ identify the selected feature type
 #-f bam if we want to use BAM format instead of SAM [default]
 
-fnames=` ls -1 *.sam | paste -sd "\t" - ` #falta adicionar gene_id ao início
+fnames=` ls -1 *sam | paste -sd "\t" - `       #Store names of the sam files, separated by tab
+echo "Building HTSeq-count tables for $fnames" 
+fnames="gene_id\t$fnames"                      #Assigning gene_id to the first column
 
+echo
+echo "Building HTseq-count tables for exon feature"
 htseq-count -t exon --stranded=no -i gene_id $files $gtf > $OUTDir"/exon.txt"
-echo -e $fnames | cat - $OUTDir"/exon.txt" > $OUTDir"/exon_header.txt"
+echo -e $fnames | cat - $OUTDir"/exon.txt" > $OUTDir"/exon_header.txt" #Assigning fnames to the header of output file
 
-#htseq-count -t CDS --stranded=no -i gene_id $files $gtf > $OUTDir"/CDS.txt"
-#htseq-count -t start_codon --stranded=no -i gene_id $files $gtf > $OUTDir"/start_codon.txt"
-#htseq-count -t stop_codon --stranded=no -i gene_id $files $gtf > $OUTDir"/stop_codon.txt"
+echo
+echo "Building HTseq-count tables for CDS feature"
+htseq-count -t CDS --stranded=no -i gene_id $files $gtf > $OUTDir"/CDS.txt"
+echo -e $fnames | cat - $OUTDir"/CDS.txt" > $OUTDir"/CDS_header.txt"
 
-#htseq-count -t CDS --stranded=no -i ID --additional-attr=protein_id $files $gtf > $OUTDir"CDS.txt"
-#htseq-count -t chromosome --stranded=no -i gene_id $files $gtf > $OUTDir"chromosome.txt"
+echo
+echo "Building HTseq-count tables for start_codon feature"
+htseq-count -t start_codon --stranded=no -i gene_id $files $gtf > $OUTDir"/start_codon.txt"
+echo -e $fnames | cat - $OUTDir"/start_codon.txt" > $OUTDir"/start_codon_header.txt"
 
-#htseq-count -t exon --stranded=no -i exon_id $files $gtf > $OUTDir"exon.txt"
+echo
+echo "Building HTseq-count tables for stop_codon feature"
+htseq-count -t stop_codon --stranded=no -i gene_id $files $gtf > $OUTDir"/stop_codon.txt"
+echo -e $fnames | cat - $OUTDir"/stop_codon.txt" > $OUTDir"/stop_codon_header.txt"
 
-#htseq-count -t five_prime_UTR --stranded=no -i Parent $files $gtf > $OUTDir"5primeUTR.txt"
-
-#htseq-count -t gene --stranded=no -i gene_id $files $gtf > $OUTDir"gene.txt"
-#htseq-count -t gene --stranded=no -i gene_id --nonunique all $files $gtf > $OUTDir"gene_nonuniqueALL.txt"
-
-#htseq-count -t lnc_RNA --stranded=no -i ID --additional-attr=transcript_id $files $gtf > $OUTDir"lncRNA.txt"
-
-#htseq-count -t miRNA --stranded=no -i ID --additional-attr=transcript_id $files $gtf > $OUTDir"microRNA.txt"
-#htseq-count -t mRNA --stranded=no -i ID --additional-attr=transcript_id $files $gtf > $OUTDir"mRNA.txt"
-
-#htseq-count -t ncRNA --stranded=no -i gene_id --additional-attr=transcript_id $files $gtf > $OUTDir"ncRNA.txt"
-#htseq-count -t ncRNA_gene --stranded=no -i gene_id --additional-attr=gene_id $files $gtf > $OUTDir"ncRNAgene.txt"
-
-#htseq-count -t pseudogene --stranded=no -i gene_id $files $gtf > $OUTDir"pseudogene.txt"
-#htseq-count -t pseudogenic_transcript --stranded=no -i gene_id --additional-attr=transcript_id $files $gtf > $OUTDir"pseudogenicTranscript.txt"
-
-#htseq-count -t rRNA --stranded=no -i gene_id --additional-attr=transcript_id $files $gtf > $OUTDir"rRNA.txt"
-
-#htseq-count -t scaffold --stranded=no -i gene_id $files $gtf > $OUTDir"scaffold.txt"
-#htseq-count -t scRNA --stranded=no -i gene_id --additional-attr=transcript_id $files $gtf > $OUTDir"scRNA.txt"
-#htseq-count -t snoRNA --stranded=no -i gene_id --additional-attr=transcript_id $files $gtf > $OUTDir"snoRNA.txt"
-#htseq-count -t snRNA --stranded=no -i gene_id --additional-attr=transcript_id $files $gtf > $OUTDir"snRNA.txt"
-
-#htseq-count -t three_prime_UTR --stranded=no -i Parent $files $gtf > $OUTDir"3primeUTR.txt"
-#htseq-count -t tRNA --stranded=no -i gene_id --additional-attr=transcript_id $files $gtf > $OUTDir"tRNA.txt"
-
-#htseq-count -t unconfirmed_transcript --stranded=no -i gene_id --additional-attr=transcript_id $files $gtf > $OUTDir"unconfTranscript.txt"
-
+echo
+echo "All HTSeq-count tables were built!"
 echo
